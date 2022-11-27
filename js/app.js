b326270104963f1da8f5168c9f7b8cd13d30ebc9
verygoodalex <agavrylov8@gmail.com>
2022-11-27 13:21:16 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$('.spoiler').hide();
+
+$('.spoilerButton').on('click', function () {
+  $('.spoilerButton').hide();
+  $('.spoiler').slideDown(2000);
+});
+$('.spoiler').hide();
+
+$('.spoilerButton1').on('click', function () {
+  $('.spoilerButton1').hide();
+  $('.spoiler1').slideDown(2000);
+});
+$('.spoiler1').hide();
+
